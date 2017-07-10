612009b1bef448d5444b66a3f38043cdcd361d92
Jasper De Moor <jdemo@live.be>
2017-07-10 17:57:53 +0200

    fix languages

--- src/app/components/connections/connectionDetail.component.ts
+++ src/app/components/connections/connectionDetail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Location } from '@angular/common';
 
+import { Language } from '../../classes/language';
+
 @Component({
     selector: 'connectiondetail',
     templateUrl: './templates/connectionDetail.component.html'
@@ -9,6 +11,7 @@ import { Location } from '@angular/common';
 export class ConnectionDetail {
     percentage = Math.floor(Math.random() * 100);
     train = 'P23412';
+    language: Language = new Language();
 
     constructor(private location: Location) {}
 
