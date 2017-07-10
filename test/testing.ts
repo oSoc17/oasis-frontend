52b9590fceb6d30b14bd34fd6c0baf50fa6a87aa
BrunoHaspeslagh <bruno.haspeslagh@student.howest.be>
2017-07-10 10:45:53 +0200

    import Connection class into testing file

--- test/testing.ts
+++ test/testing.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
-import { Connection } from "app/classes/Connection";
-/*-------------------------------------------------------------
+import {Connection} from ''
+/*---------------------------------------------------------------------------------------------------------------------------------------
 Conventions for writing tests:
 - the name of the file where the piece of code that is being tested should be mentioned in the name of the test
 - describe scenario above test
 - tests should be devided into 3 distinct sections: setup, execution and assertion
----------------------------------------------------------------*/
+----------------------------------------------------------------------------------------------------------------------------------------*/
 
 /* Example test */
 describe('1+1 test', () => {
@@ -15,7 +15,7 @@ describe('1+1 test', () => {
   });
 });
 
-describe('Connection.ts Connection class constructor test' ,()=>{
+describe('Connection.ts: Connection class constructor test' ,()=>{
   //setup
   const dummyjson = '{"@id": "#1499679000000881434088____%3A007%3A%3A8841004%3A8884335%3A52%3A1247%3A20170710","@type": "Connection","departureStop": "http://irail.be/stations/NMBS/008814340","arrivalStop": "http://irail.be/stations/NMBS/008814357","departureTime": "2017-07-10T09:30:00.000Z","arrivalTime": "2017-07-10T09:30:00.000Z","gtfs:trip": "http://irail.be/trips/88____%3A007%3A%3A8841004%3A8884335%3A52%3A1247%3A20170710","gtfs:route": "http://irail.be/routes/51"}';
   const json = JSON.parse(dummyjson);
