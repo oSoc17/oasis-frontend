19261e676ad4d5c4ebf65bf92f8387908e42fbf4
Robrecht <robrechtmeersman@hotmail.com>
2017-07-10 13:04:02 +0200

    test & bug fix Route

--- src/app/classes/Route.ts
+++ src/app/classes/Route.ts
@@ -1,34 +1,31 @@
 import {Connection} from "./Connection";
 
 export class Route {
-  private connections: Connection[] = [];
+  connections: Connection[] = [];
 
-  public getQoE() {
+  public getQoE(): number {
     /* returns the  global QoE percentage of the route */
     if (this.connections.length == 0) {
       throw new Error("Route is empty");
     } else {
       let QoE: number = 0;
       let totalTravelTime: number = 0;
-      let n = 1;
+      let n: number = 0;
       for (let c of this.connections) {
         // iterative avg calculation algorithm
-        let travelTime = c.arrivalTime.valueOf() - c.departureTime.valueOf();
-        QoE = QoE + (c.calculateQoE() * travelTime - QoE) / (n * totalTravelTime);
-        n++;
+        let travelTime:number = c.arrivalTime.valueOf() - c.departureTime.valueOf();
         totalTravelTime += travelTime;
+        n++;
+        QoE = QoE + (c.calculateQoE() * travelTime - QoE) / (n * totalTravelTime);
+        //console.log("QoE: "+QoE);
       }
       return QoE;
     }
   }
 
-  public addConnection(c: Connection) {
-    /* appends a Connection to the array */
-    this.connections.push(c);
-  }
-
   public getDataValues() {
     /* returns components of QoE */
+    //TODO: return seperate values from QoE calculation
   }
 
 }
