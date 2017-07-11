5b4a0fa66e9abb8a7daf3f1835daf0712f81a9bd
BrunoHaspeslagh <bruno.haspeslagh@student.howest.be>
2017-07-11 09:37:58 +0200

    add first draft of queryPeriod function

--- src/app/services/routing.service.ts
+++ src/app/services/routing.service.ts
@@ -6,6 +6,10 @@ const Client = require('lc-client');
 
 export class RouteService {
     private planner;
+     private static zeroPad(num: number, places: number) {
+        const zero = places - num.toString().length + 1;
+        return Array(+(zero > 0 && zero)).join('0') + num;
+    }
 
     // example: ['http://belgianrail.linkedconnections.org/']
     constructor(entryPoints: [string]) {
@@ -38,18 +42,34 @@ export class RouteService {
         });
     }
     /**
-     * does multiple queries with the same configuration. Only the date is different.
+     * Does multiple queries with the same configuration. Only the date is different.
      * The time between query dates is determened by the second parameter
      * @param searchData object containing information regarding the query
      * @param period time in days between the dates to query
      * @returns an array of query results
      */
-    queryPeriod(searchData: SearchData, period: number ): Promise<any> {
+    queryPeriod(searchdata: SearchData, period: number ): Promise<any> {
         return new Promise((resolve, reject) => {
+            const searchData = searchdata;
+            const ret = [];
+            let day = 0;
+            let month = 0;
+            let year = 0;
+            let newdate = '';
+            while (ret.length < 10) {
+                ret.push(this.query(searchData));
+                day = Number(searchData.travelDate.split('/')[0]);
+                month = Number(searchData.travelDate.split('/')[1]);
+                year = Number(searchData.travelDate.split('/')[2]);
+                day -= period;
+                newdate = RouteService.zeroPad(day, 2) + '/' + RouteService.zeroPad(month, 2) + '/' + RouteService.zeroPad(year, 2);
+                searchData.travelDate = newdate;
+            }
 
+
+            return ret;
         });
     }
-
     private handleError(error: any): Promise<any> {
         return Promise.reject(error.message || error);
     }
