b4296dd5840a7bdfb2c81fff1ec8c23b21e6e561
Robrecht <robrechtmeersman@hotmail.com>
2017-07-11 08:05:06 +0200

    refactor

--- src/app/classes/language.ts
+++ src/app/classes/language.ts
@@ -1,66 +1,66 @@
 export class Language {
-  public static language = 'en_GB';
+    public static language = 'en_GB';
 
-  getLanguageData() {
-    // returns locales/[currentLanguage].json
-    try {
-      const language = require(`../../locales/${Language.language}.json`);
-      return language;
-    } catch (e) {
-      console.log(e);
-      return null;
+    getLanguageData(): string {
+        // returns locales/[currentLanguage].json
+        try {
+            const language = require(`../../locales/${Language.language}.json`);
+            return language;
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
     }
-  }
 
-  getMessage(key: string) {
-    // returns message in current language
-    const lang = this.getLanguageData();
-    if (lang) {
-      return lang[key];
+    getMessage(key: string): string {
+        // returns message in current language
+        const lang = this.getLanguageData();
+        if (lang) {
+            return lang[key];
+        }
+        return null;
     }
-    return null;
-  }
 
-  getLanguages(): string[] {
-    // returns locales/languages.json
-    try {
-      const file = require('../../locales/languages.json');
-      return file['languages'];
-    } catch (e) {
-      console.log(e);
-      return null;
+    getLanguages(): string[] {
+        // returns locales/languages.json
+        try {
+            const file = require('../../locales/languages.json');
+            return file['languages'];
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
     }
-  }
 
-  setLanguage(tag: string) {
-    // checks if language appears in locales/languages.json and selects if so
-    const languages = this.getLanguages();
-    for(let pair of languages) {
-      if(pair['tag'] == tag) {
-        Language.language = tag;
-      }
+    setLanguage(tag: string) {
+        // checks if language appears in locales/languages.json and selects if so
+        const languages = this.getLanguages();
+        for(let pair of languages) {
+            if(pair['tag'] == tag) {
+                Language.language = tag;
+            }
+        }
     }
-  }
 
-  toTag(name: string): string {
-    // returns ISO tag for a language name
-    const languages: string[] = this.getLanguages();
-    for(let pair of languages) {
-      if(pair['name'] == name) {
-        return pair['tag'];
-      }
+    toTag(name: string): string {
+        // returns ISO tag for a language name
+        const languages: string[] = this.getLanguages();
+        for(let pair of languages) {
+            if(pair['name'] == name) {
+                return pair['tag'];
+            }
+        }
+        return null;
     }
-    return null;
-  }
 
-  toName(tag: string): string {
-    // returns a readable name for the language ISO tag
-    const languages: string[] = this.getLanguages();
-    for(let pair of languages) {
-      if(pair["tag"] == tag) {
-        return pair["name"];
-      }
+    toName(tag: string): string {
+        // returns a readable name for the language ISO tag
+        const languages: string[] = this.getLanguages();
+        for(let pair of languages) {
+            if(pair["tag"] == tag) {
+                return pair["name"];
+            }
+        }
+        return null;
     }
-    return null;
-  }
 }
