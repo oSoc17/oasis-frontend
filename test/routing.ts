5b45433621004f4e631fa7b464c6936d854c9010
Jasper De Moor <jdemo@live.be>
2017-07-11 12:11:42 +0200

    fixed it

--- test/routing.ts
+++ test/routing.ts
@@ -7,17 +7,17 @@ import { RouteService } from '../src/app/services/routing.service';
 import { SearchData } from '../src/app/classes/searchData';
 import 'rxjs/add/operator/toPromise';
 
-
-
 describe('RoutingService test', () => {
-    it('RoutingService#query()', () => {
+    it('RoutingService#query()', (done) => {
         const routeService = new RouteService(['http://belgium.linkedconnections.org/sncb/connections']);
         const searchData = new SearchData('http://irail.be/stations/NMBS/008892007',
                 'http://irail.be/stations/NMBS/008812005', '11:35', '10/07/2017', 'departureTime');
-        routeService.query(searchData);
-        /*expect(routeService.query(searchData)).to.eventually.to.be.an('array').to.have.deep.property('arrivalStop',
-                    { arrivalStop: 'http://irail.be/stations/NMBS/008821964' }).notify(done);*/
-        return Chai.assert.eventually.deepProperty(routeService.query(searchData),
-            'arrivalStop', 'arrivalStop is a property');
-    }); // .timeout(5000)
+
+        routeService.query(searchData).then((data) => {
+            if (data[0].arrivalStop) {
+                return done();
+            }
+            done('No arrivalStop value in first item of array');
+        }).catch(e => done(e));
+    }).timeout(5000);
 });
