bcdf6b9d6a272946c4a6326686f6e87ec97b8dad
Robrecht <robrechtmeersman@hotmail.com>
2017-07-12 10:36:29 +0200

    implement IQoE

--- src/app/classes/qoe.ts
+++ src/app/classes/qoe.ts
@@ -1,9 +1,47 @@
 import { RouteHistory } from './routeHistory';
+import { IQoE } from '../interfaces/iQoE';
 
-export class QoE {
+export class QoE implements IQoE {
+
     routeHistory: RouteHistory;
 
     constructor(routeHistory: RouteHistory) {
         this.routeHistory = routeHistory;
+    }
+
+    getAvgDelay(): Date {
+        return undefined;
+    }
+
+    getAvgChangesAmount(): Date {
+        return undefined;
+    }
+
+    getAvgChangeTime(): Date {
+        return undefined;
+    }
+
+    getDelayConsistency(): Date {
+        return undefined;
+    }
+
+    getAvgTravelTime(): Date {
+        return undefined;
+    }
+
+    getNumberOfRoutesWithinHour(): number {
+        return undefined;
+    }
+
+    getNumberOfMissedConnections(): number {
+        return undefined;
+    }
+
+    getPrice(): number {
+        return undefined;
+    }
+
+    getQoE(weights): number {
+        return undefined;
     }
 }
