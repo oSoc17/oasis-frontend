21da10d4e65e6219042b531c472ea2f34dd45c1d
Robrecht <robrechtmeersman@hotmail.com>
2017-07-12 12:09:26 +0200

    implement qoe methods

--- src/app/classes/qoe.ts
+++ src/app/classes/qoe.ts
@@ -13,10 +13,10 @@ export class QoE implements IQoE {
     }
 
     getAvgDelay(): any {
-        const delay: number = this.routeHistory.getAvgDelay().valueOf() / 60000;
-        // TODO: review QoE calculation
-        // QoE(Delay) = 1 - delay / worst case delay
-        const score = this.userPreferences.weight_AvgDelay * (1 - delay / 60);
+        const delay: number = this.routeHistory.getAvgDelay().valueOf() / 60000; // in minutes
+        const weight: number = this.userPreferences.weight_AvgDelay;
+        // QoE(delay) = 1 - delay / worst case delay
+        const score = weight * (1 - delay / 60);
         return {
             score: score,
             value: delay
@@ -24,34 +24,74 @@ export class QoE implements IQoE {
     }
 
     getAvgChangesAmount(): any {
-        return undefined;
+        const changes: number = this.routeHistory.getAvgChangesAmount();
+        const weight: number = this.userPreferences.weight_AvgChangesAmount;
+        // QoE(changes) = 1 - changes / worst case changes
+        const score: number = weight * (1 - changes / 5);
+        return {
+            score: score,
+            value: changes
+        };
     }
 
     getAvgChangeTime(): any {
-        return undefined;
+        const changeTime: number = this.routeHistory.getAvgChangeTime().valueOf() / 60000; // in minutes
+        const weight: number = this.userPreferences.weight_AvgChangeTime;
+        // QoE(changeTime) = 0 if less than 3 min., otherwise compare to 20 min.
+        const score: number = weight * (changeTime < 3 ? 0 : 1 - ((changeTime - 3) / 17));
+        return {
+            score: score,
+            value: changeTime
+        };
     }
 
     getDelayConsistency(): any {
-        return undefined;
+        const delayConsistency: number = this.routeHistory.getDelayConsistency().valueOf() / 60000; // in minutes
+        const weight: number = this.userPreferences.weight_DelayConsistency;
+        // QoE(stdDev) = 1 - stdDev / worst case stdDev
+        const score = weight * (1 - delayConsistency / 60);
+        return {
+            score: score,
+            value: delayConsistency
+        };
     }
 
     getAvgTravelTime(): any {
-        return undefined;
+        const travelTime: number = this.routeHistory.getAvgTravelTime().valueOf() / 60000; // in minutes
+        const weight: number = this.userPreferences.weight_AvgTravelTime;
+        // QoE(travelTime) = TODO: figure out formula
+        const score = weight * (travelTime / 60);
+        return {
+            score: score,
+            value: travelTime
+        };
     }
 
     getNumberOfRoutesWithinHour(): any {
+        // TODO: modify lc-client
         return undefined;
     }
 
     getNumberOfMissedConnections(): any {
+        // TODO: modify lc-client
         return undefined;
     }
 
     getPrice(): any {
+        // TODO
         return undefined;
     }
 
-    getQoE(weights): any {
-        return undefined;
+    getQoE(weights): number {
+        let sum = 0;
+        sum += this.getAvgTravelTime();
+        sum += this.getAvgChangeTime();
+        sum += this.getAvgChangesAmount();
+        sum += this.getDelayConsistency();
+        sum += this.getAvgDelay();
+        // sum += this.getNumberOfMissedConnections();
+        // sum += this.getNumberOfRoutesWithinHour();
+        // sum += this.getPrice();
+        return sum;
     }
 }
