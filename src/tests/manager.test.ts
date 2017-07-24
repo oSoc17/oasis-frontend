b409f9c54469d359d6784a4544d0d37957266f90
BrunoHaspeslagh <bruno.haspeslagh@student.howest.be>
2017-07-24 11:09:42 +0200

    all tests get executed

--- src/tests/manager.test.ts
+++ src/tests/manager.test.ts
@@ -11,12 +11,12 @@ describe('Manager test', () => {
     it('Manager#getQoE()', (done) => {
         const searchData = new SearchData('http://irail.be/stations/NMBS/008896115',
                     'http://irail.be/stations/NMBS/008821006', '12:30', '10/07/2017', '');
-        const manager = new Manager([config.entrypoints['belgium_sncb']]);
+        const manager = new Manager([config.entrypoints['belgium_sncb'].uri]);
         manager.getQoE([searchData], false);
-        // manager.getRouteListener.subscribe(e => {
-        //     const qoe = manager.qoeList[0];
-        //     expect(qoe).toEqual(jasmine.any(QoE));
-        //     done();
-        // });
+        manager.getRouteListener.subscribe(e => {
+            const qoe = manager.qoeList[0];
+            expect(qoe).toEqual(jasmine.any(QoE));
+            done();
+        });
     }, 15000);
 });
