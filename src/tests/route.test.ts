9673a4e4ff13d9e2b351d56f6c93b29e35e32d4f
BrunoHaspeslagh <bruno.haspeslagh@student.howest.be>
2017-07-24 11:59:54 +0200

    various bugfixes and test cleanup close #276

--- src/tests/route.test.ts
+++ src/tests/route.test.ts
@@ -100,14 +100,12 @@ describe('Route.ts getChangesAmount()', () => {
         const json1 = JSON.parse('{"@id": "#1499679000000881434088____%3A007%3A%3A8841004%3A8884335%3A52%3A1247%3A20170' +
         '710","@type": "Connection","departureStop": "http://irail.be/stations/NMBS/008814340","arrivalStop": "http://i' +
         'rail.be/stations/NMBS/008814357","departureTime": "2017-07-10T09:30:00.000Z","arrivalTime": "2017-07-10T09:40:00' +
-        '.000Z","http://vocab.gtfs.org/terms#trip": "http://irail.be/trips/88____%3A007%3A%3A8841004%3A8884335%3A52%3A1247%' +
-        '3A20170710","http://vocab.gtfs.org/terms#route": "http://irail.be/routes/51"}');
+        '.000Z","http://vocab.gtfs.org/terms#trip": "A","http://vocab.gtfs.org/terms#route": "http://irail.be/routes/51"}');
         const c1 = new Connection(json1);
         const json2 = JSON.parse('{"@id": "#1499679000000881434088____%3A007%3A%3A8841004%3A8884335%3A52%3A1247%3A2017071' +
         '1","@type": "Connection","departureStop": "http://irail.be/stations/NMBS/008814357","arrivalStop": "http://irail' +
         '.be/stations/NMBS/008814358","departureTime": "2017-07-10T09:42:00.000Z","arrivalTime": "2017-07-10T09:50:00.000Z' +
-        '","http://vocab.gtfs.org/terms#trip": "http://irail.be/trips/88____%3A007%3A%3A8841004%3A8884335%3A52%3A1247%3A' +
-        '20170711","http://vocab.gtfs.org/terms#route": "http://irail.be/routes/51"}');
+        '","http://vocab.gtfs.org/terms#trip": "B","http://vocab.gtfs.org/terms#route": "http://irail.be/routes/51"}');
         const c2 = new Connection(json2);
         const route = new Route([]);
         route.addConnection(c1);
