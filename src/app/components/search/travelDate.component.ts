a27aaad2046257b6c5fbabde612ddbf327dc9b93
Robrecht <robrechtmeersman@hotmail.com>
2017-07-25 09:48:50 +0200

    add documentation

--- src/app/components/search/travelDate.component.ts
+++ src/app/components/search/travelDate.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+// Node modules
+import { Component } from '@angular/core';
 
+// Custom modules
 import { Language } from '../../classes/userData/language';
 
 @Component({
@@ -8,12 +10,15 @@ import { Language } from '../../classes/userData/language';
     styleUrls: ['./styles/travelDate.component.scss']
 })
 
+/**
+ * A form combobox to select a day of the week
+ */
 export class TravelDate {
     selectedDay: number = (new Date()).getDay(); // weekday index starting at sunday (0)
     language = new Language();
 
     /**
-     * Sets the traveldate to the current pc's date
+     * Set the selectedDay index to the current machines day
      */
     setNow() {
         this.selectedDay = (new Date()).getDay();
--- src/app/interfaces/iQoE.ts
+++ src/app/interfaces/iQoE.ts
@@ -1,3 +1,6 @@
+/**
+ * Interface for QoE class, used for mocking
+ */
 export interface IQoE {
 
     getAvgDelay(): any
